c6aa8c02f26f7a4f197eaafc7c7087aa6af3e119
Fernando R. Guzman Casas <fernando.guzman2@upr.edu>
2020-05-13 17:27:45 -0400

    second commit:

---
+++ MainMenuUI.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+
+HEIGHT = 380
+WIDTH = 600
+
+root = tk.Tk()
+
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame = tk.Frame(root)
+frame.place(relx=.15, rely=.1)
+capture_mode_button = tk.Button(frame, text="Capture Mode", width=18, bg='orange')
+capture_mode_button.pack()
+
+frame = tk.Frame(root)
+frame.place(relx=.6, rely=.1)
+gallery_button = tk.Button(frame, text="Gallery", width=18, bg='green')
+gallery_button.pack()
+
+frame = tk.Frame(root)
+frame.place(relx=.15, rely=.6)
+sensor_mode_button = tk.Button(frame, text="Sensor Mode", width=18, bg='blue')
+sensor_mode_button.pack()
+
+frame = tk.Frame(root)
+frame.place(relx=.6, rely=.6)
+diagnostics_button = tk.Button(frame, text="Diagnostics", width=18, bg='red')
+diagnostics_button.pack()
+
+frame = tk.Frame(root)
+frame.place(relx=.8, rely=.8)
+back_button = tk.Button(frame, text="Back", width=10, bg='purple')
+back_button.pack()
+
+
+root.mainloop()
